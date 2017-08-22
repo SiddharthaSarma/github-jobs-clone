f38325ee73c1e367404f77d47b93ba0317bf5bf9
siddharthasarma <sidduinevitable@gmail.com>
2017-08-22 08:15:08 +0530

    Made few changes in pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,20 @@
 <template>
   <section class="container is-fluid">
+    <Navbar></Navbar>
     <Search></Search>
     <JobsList></JobsList>
   </section>
 </template>
 
 <script>
+
+import Navbar from '~/components/Navbar';
 import Search from '~/components/Search';
 import JobsList from '~/components/JobsList';
 
 export default {
   components: {
+    Navbar,
     Search,
     JobsList
   }
