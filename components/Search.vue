c54682a94f5bf7e4c779cf732b1c3e65fba18fba
siddharthasarma <sidduinevitable@gmail.com>
2017-08-22 08:15:26 +0530

    Made few changes in the components files

--- components/Search.vue
+++ components/Search.vue
@@ -3,24 +3,24 @@
     <form>
       <div class="columns">
         <div class="column field">
-          <label class="label">Name</label>
+          <label class="label">Job description</label>
           <div class="control">
-            <input class="input" type="text" placeholder="e.g Alex Smith">
+            <input class="input" type="text" placeholder="Filter by title, benefits, companies, expertise">
           </div>
         </div>
         <div class=" column">
-          <label class="label">Email</label>
+          <label class="label">Location</label>
           <div class="control">
-            <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com">
+            <input class="input" type="text" placeholder="Filter by city, state, zip code or country">
           </div>
         </div>
-        <div class="column is-2">
+        <div class="column is-2 checkbox-container">
           <label class="checkbox">
             <input type="checkbox">
             Remember me
           </label>
         </div>
-        <div class="column is-2">
+        <div class="column is-2 submit-container">
           <button class="button is-primary" type="submit">Search</button>
         </div>
       </div>
@@ -34,6 +34,13 @@
   };
 </script>
 
-<style>
-
+<style scoped>
+.submit-container {
+  display: flex;
+  align-items: center;
+  -webkit-align-items: center;
+}
+  .checkbox-container label{
+    line-height: 104px;
+  }
 </style>
