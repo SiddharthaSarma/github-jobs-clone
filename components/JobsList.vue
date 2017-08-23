c1b4f56c71aba2180e0ee6ea339ff76977feffce
siddharthasarma <sidduinevitable@gmail.com>
2017-08-23 08:33:41 +0530

    Made necessary modifications to JobsList.vue

--- components/JobsList.vue
+++ components/JobsList.vue
@@ -1,30 +1,25 @@
 <template>
   <div class="container is-fluid">
-    <div class="box">
+    <div class="box" v-for="job in jobsList" v-bind:key="job.id">
       <article class="media">
         <div class="media-left">
           <figure class="image is-64x64">
-            <img src="http://bulma.io/images/placeholders/128x128.png" alt="Image">
+            <img :src="job.company_logo" alt="Image">
           </figure>
         </div>
         <div class="media-content">
           <div class="content">
-            <p>
-              <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
-              <br>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-            </p>
+              <h4>
+                {{job.title}}
+                <span class="media-right is-pulled-right tag is-dark">{{job.type}}</span>
+              </h4>
+            <!--<p v-html="job.description"></p>-->
+            <p class="location"><i class="fa fa-map-marker"></i> {{job.location}}</p>
           </div>
           <nav class="level is-mobile">
             <div class="level-left">
               <a class="level-item">
                 <span class="icon is-small"><i class="fa fa-reply"></i></span>
-              </a>
-              <a class="level-item">
-                <span class="icon is-small"><i class="fa fa-retweet"></i></span>
-              </a>
-              <a class="level-item">
-                <span class="icon is-small"><i class="fa fa-heart"></i></span>
               </a>
             </div>
           </nav>
@@ -35,7 +30,43 @@
 </template>
 
 <script>
-  export default{
-    name: 'jobslist'
+  import axios from 'axios';
+  export default {
+    name: 'jobslist',
+    props:['description', 'location'],
+    data() {
+      return {
+          jobsList: []
+      }
+    },
+    methods: {
+        fetchJobs() {
+          axios.get(`https://jobs.github.com/positions.json?description=${this.description}&location=${this.location}`).then(function(response) {
+            this.jobsList = response.data;
+          }.bind(this));
+        }
+    },
+    mounted() {
+      this.fetchJobs();
+    },
+    watch: {
+        description: function(val) {
+            this.description = val;
+            this.fetchJobs();
+        },
+        location: function(val) {
+            this.location = val;
+            this.fetchJobs();
+        }
+    }
   };
 </script>
+<style scoped>
+  .image img {
+    width: 64px;
+    height: 64px;
+  }
+  .location i {
+    line-height: 24px;
+  }
+</style>
