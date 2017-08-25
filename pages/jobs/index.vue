3ac28b7e75969819c09a7dfdb27cb8b21584f151
siddharthasarma <sidduinevitable@gmail.com>
2017-08-25 18:52:22 +0530

    Added the functionality to show the individual job details.

---
+++ pages/jobs/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="user">
+    <h3>{{ name }}</h3>
+    <h4>@{{ username }}</h4>
+    <p>Email : {{ email }}</p>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+
+//    async asyncData ({ params, error }) {
+//      try {
+//        const { data } = await axios.get(`https://jobs.github.com/positions/87461912-8741-11e7-9123-2e5a5789abc2.json`);
+//        return data
+//      } catch (e) {
+//        error({ message: 'Job not found', statusCode: 404 })
+//      }
+//    }
+
+    created() {
+      axios.get(`https://jobs.github.com/positions/87461912-8741-11e7-9123-2e5a5789abc2.json`).then(function(response) {
+          console.log(response);
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .user {
+    text-align: center;
+    margin-top: 100px;
+    font-family: sans-serif;
+  }
+</style>
